9d0e4c1097bb13d68cde3e815c27c785f60e6b65
BonoDV <84352335+BonoDV@users.noreply.github.com>
2025-04-09 11:06:39 +0200

    Delete non used files, done mobile header

---
+++ src/components/Header.jsx
@@ -0,0 +1,67 @@
+
+import SearchBar from "./SearchBar"
+import "../styles/Header.css";
+function Header() {
+
+    return (
+        <>
+            <header style={headerStyle}>
+                <div style={headerDiv}>
+                    <img src="./assets/logo.png" alt="Logo" className="logo" style={logoStyle} />
+                    <SearchBar style={searchBarStyle} />
+                </div>
+                <div className="header__navbar" style={headerNavBarStyle}>
+                    <ul style={headerNavBarUlStyle}>
+                        <li style={{ marginRight: '61px' }}>ALL</li>
+                        <li>MY PHOTOS</li>
+                    </ul>
+                </div>
+            </header>
+        </>
+    )
+}
+
+export default Header
+
+const headerStyle = {
+    backgroundImage: 'url("./assets/headerBackground.jpg")',
+    width: '100%',
+    height: '230px',
+    backgroundSize: 'cover',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between', // Esto empuja el navbar hacia abajo
+}
+
+const headerDiv = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+}
+
+const logoStyle = {
+    width: '116px',
+    height: '116px',
+    marginLeft: '20px',
+    marginTop: '20px',
+}
+
+const searchBarStyle = {
+    marginTop: '20px'
+}
+
+const headerNavBarStyle = {
+    display: 'flex',
+    justifyContent: 'center', // Centra el navbar horizontalmente
+    cursor: 'pointer',
+}
+
+const headerNavBarUlStyle = {
+    listStyleType: 'none',
+    display: 'flex',
+    padding: '0',
+    color: '#FFFFFF',
+    fontSize: '24px',
+    fontFamily: 'Inter, sans-serif',
+    fontWeight: '600',
+}
